d98c5d5bb47db7a7025480d20e8bc2dc67ea6770
Matt Cole <mcole.uk@gmail.com>
2022-01-06 15:39:04 +0000

    version 0.1

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './navigation/drawerNavigator';
+
+import productReducer from './store/reducer/products';
+import basketReducer from './store/reducer/basket';
+import orderReducter from './store/reducer/orders';
+
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+// import {composeWithDevTools} from 'redux-devtools-extension'; // dev only remove for production
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  basket: basketReducer,
+  orders: orderReducter
+});
+
+// const store = createStore(rootReducer, composeWithDevTools()); // dev only remove for production
+const store = createStore(rootReducer); // production
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <NavigationContainer>
+            <Navigation />
+        </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
